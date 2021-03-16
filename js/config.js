ed4b547921f69776d2ed9c4172044548c4a50672
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2021-03-16 20:11:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,3 @@
-// Prod
-// tenantUrl = '';
-// configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/e1b39e9a73d34950830fc2fe1199c213';
-// dataUrl = '';
-// sectionUrl = '';
-
-// Dev
-configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/eaff754433c04c7eb11553ffb155fb39';
-
 config = {
   "logo": "../os/images/icon/logo.png",
   "name": "OpenSourceTechHub",
