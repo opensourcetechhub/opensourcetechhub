d525acd0f75e44ca29c04d83abf6ec05d3852f44
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2021-03-16 20:10:23 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,3 +6,35 @@
 
 // Dev
 configUrl = 'https://jsoneditoronline.herokuapp.com/v1/docs/eaff754433c04c7eb11553ffb155fb39';
+
+config = {
+  "logo": "../os/images/icon/logo.png",
+  "name": "OpenSourceTechHub",
+  "nav": "Y",
+  "filter": "N",
+  "data": "https://jsoneditoronline.herokuapp.com/v1/docs/be485d2710cb418eaa5333f59c5fffdf"
+};
+
+sections = [ {
+  "id" : "sw",
+  "name" : "Softwares",
+  "state" : "active"
+}, {
+  "id" : "hw",
+  "name" : "Hardwares"
+}, {
+  "id" : "lang",
+  "name" : "Programming Languages"
+}, {
+  "id" : "found",
+  "name" : "Foundations"
+}, {
+  "id" : "blog",
+  "name" : "Blogs"
+}, {
+  "id" : "forum",
+  "name" : "Forums"
+}, {
+  "id" : "blog",
+  "name" : "Blogs"
+} ];
