d1f875b178f7d1fe93375a4b4ed0f77988940fd3
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2021-03-16 20:22:00 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,195 +26,173 @@ sections = [ {
   "name" : "Forums"
 } ];
 
-[
-  {
-    "section": "sw",
-    "id": "cordova",
-    "name": "Apache Cordova",
-    "desc": "",
-    "type": "preview",
-    "link": "https://cordova.apache.org/",
-    "platform" : "https://github.com/apache/cordova",
-    "size": "12"
-  },
-  {
-    "section": "sw",
-    "id": "fw7",
-    "name": "Framework 7",
-    "desc": "",
-    "type": "preview",
-    "link": "https://framework7.io/",
-    "platform" : "https://github.com/framework7io/framework7",
-    "size": "12"
-  },
-  {
-    "section": "sw",
-    "id": "ionic",
-    "name": "Ionic",
-    "desc": "",
-    "type": "preview",
-    "link": "https://ionicframework.com/",
-    "platform" : "https://github.com/ionic-team/ionic-framework",
-    "size": "12"
-  },
-  {
-    "section": "sw",
-    "id": "amp",
-    "name": "AMP",
-    "desc": "",
-    "type": "preview",
-    "link": "https://amp.dev/",
-    "platform" : "https://github.com/ampproject/amphtml",
-    "size": "12"
-  },
-  {
-    "section": "sw",
-    "id": "electron",
-    "name": "Electron",
-    "desc": "",
-    "type": "preview",
-    "link": "https://electronjs.org/",
-    "platform" : "https://github.com/electron/electron",
-    "size": "12"
-  },
-  {
-    "section": "sw",
-    "id": "grail",
-    "name": "Grail",
-    "desc": "",
-    "type": "preview",
-    "link": "https://grails.org/",
-    "platform" : "https://github.com/grails/grails-core",
-    "size": "12"
-  },
-  {
-    "section": "hw",
-    "id": "rapi",
-    "name": "Raspberry Pi",
-    "desc": "",
-    "type": "preview",
-    "link": "https://www.raspberrypi.org/",
-    "size": "12"
-  },
-  {
-    "section": "hw",
-    "id": "arduino",
-    "name": "Arduino",
-    "desc": "",
-    "type": "preview",
-    "link": "https://www.arduino.cc/",
-    "platform" : "https://github.com/arduino/Arduino",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "java",
-    "name": "Java",
-    "desc": "",
-    "type": "preview",
-    "link": "https://www.oracle.com/java/",
-    "platform" : "http://hg.openjdk.java.net/",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "js",
-    "name": "JavaScript",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "scala",
-    "name": "Scala",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "groovy",
-    "name": "Groovy",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "python",
-    "name": "Python",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "lang",
-    "id": "kotlin",
-    "name": "Kotlin",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "found",
-    "id": "apache",
-    "name": "Apache",
-    "desc": "",
-    "type": "preview",
-    "link" : "https://www.apache.org/",
-    "size": "12"
-  },
-  {
-    "section": "found",
-    "id": "eclipse",
-    "name": "Eclipse",
-    "desc": "",
-    "type": "preview",
-    "link" : "https://www.eclipse.org/",
-    "size": "12"
-  },
-  {
-    "section": "found",
-    "id": "linux",
-    "name": "The Linux Foundation",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "found",
-    "id": "oc",
-    "name": "Object Computing",
-    "desc": "",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "blog",
-    "id": "dzone",
-    "name": "DZone",
-    "desc": "",
-    "link": "https://dzone.com",
-    "type": "preview",
-    "size": "12"
-  },
-  {
-    "section": "blog",
-    "id": "infoq",
-    "name": "InfoQ",
-    "desc": "",
-    "type": "preview",
-    "link" : "https://infoq.com",
-    "size": "12"
-  },
-  {
-    "section": "forum",
-    "id": "sof",
-    "name": "StackOverlow",
-    "desc": "",
-    "type": "preview",
-    "link" : "https://stackoverflow.com",
-    "size": "12"
-  }
-]
+widgets = [ {
+  "section": "sw",
+  "id": "cordova",
+  "name": "Apache Cordova",
+  "desc": "",
+  "type": "preview",
+  "link": "https://cordova.apache.org/",
+  "platform" : "https://github.com/apache/cordova",
+  "size": "12"
+}, {
+  "section": "sw",
+  "id": "fw7",
+  "name": "Framework 7",
+  "desc": "",
+  "type": "preview",
+  "link": "https://framework7.io/",
+  "platform" : "https://github.com/framework7io/framework7",
+  "size": "12"
+}, {
+  "section": "sw",
+  "id": "ionic",
+  "name": "Ionic",
+  "desc": "",
+  "type": "preview",
+  "link": "https://ionicframework.com/",
+  "platform" : "https://github.com/ionic-team/ionic-framework",
+  "size": "12"
+}, {
+  "section": "sw",
+  "id": "amp",
+  "name": "AMP",
+  "desc": "",
+  "type": "preview",
+  "link": "https://amp.dev/",
+  "platform" : "https://github.com/ampproject/amphtml",
+  "size": "12"
+}, {
+  "section": "sw",
+  "id": "electron",
+  "name": "Electron",
+  "desc": "",
+  "type": "preview",
+  "link": "https://electronjs.org/",
+  "platform" : "https://github.com/electron/electron",
+  "size": "12"
+}, {
+  "section": "sw",
+  "id": "grail",
+  "name": "Grail",
+  "desc": "",
+  "type": "preview",
+  "link": "https://grails.org/",
+  "platform" : "https://github.com/grails/grails-core",
+  "size": "12"
+}, {
+  "section": "hw",
+  "id": "rapi",
+  "name": "Raspberry Pi",
+  "desc": "",
+  "type": "preview",
+  "link": "https://www.raspberrypi.org/",
+  "size": "12"
+}, {
+  "section": "hw",
+  "id": "arduino",
+  "name": "Arduino",
+  "desc": "",
+  "type": "preview",
+  "link": "https://www.arduino.cc/",
+  "platform" : "https://github.com/arduino/Arduino",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "java",
+  "name": "Java",
+  "desc": "",
+  "type": "preview",
+  "link": "https://www.oracle.com/java/",
+  "platform" : "http://hg.openjdk.java.net/",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "js",
+  "name": "JavaScript",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "scala",
+  "name": "Scala",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "groovy",
+  "name": "Groovy",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "python",
+  "name": "Python",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "lang",
+  "id": "kotlin",
+  "name": "Kotlin",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "found",
+  "id": "apache",
+  "name": "Apache",
+  "desc": "",
+  "type": "preview",
+  "link" : "https://www.apache.org/",
+  "size": "12"
+}, {
+  "section": "found",
+  "id": "eclipse",
+  "name": "Eclipse",
+  "desc": "",
+  "type": "preview",
+  "link" : "https://www.eclipse.org/",
+  "size": "12"
+}, {
+  "section": "found",
+  "id": "linux",
+  "name": "The Linux Foundation",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "found",
+  "id": "oc",
+  "name": "Object Computing",
+  "desc": "",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "blog",
+  "id": "dzone",
+  "name": "DZone",
+  "desc": "",
+  "link": "https://dzone.com",
+  "type": "preview",
+  "size": "12"
+}, {
+  "section": "blog",
+  "id": "infoq",
+  "name": "InfoQ",
+  "desc": "",
+  "type": "preview",
+  "link" : "https://infoq.com",
+  "size": "12"
+}, {
+  "section": "forum",
+  "id": "sof",
+  "name": "StackOverlow",
+  "desc": "",
+  "type": "preview",
+  "link" : "https://stackoverflow.com",
+  "size": "12"
+} ];
